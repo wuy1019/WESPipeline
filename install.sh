79753b27a9d20072c06d0710ee4831bfeda9fce3
wuy1019 <wuy1019@gmail.com>
2018-09-06 19:12:23 +0800

    fix

---
+++ install.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# File Name: install.sh
+# Created Time: Thu 06 Sep 2018 07:08:08 PM CST
+#########################################################################
+#!/bin/bash
+ln -s src/Basic.py basic.py
+ln -s src/somatic/pipe.py somatic.py
+chmod 755 basic.py somatic.py
